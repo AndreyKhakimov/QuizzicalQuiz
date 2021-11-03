c299c9f77336c97ef2e25eed964ef7534db15377
Andrey Khakimov <khakimov013@gmail.com>
2021-11-03 17:37:52 +0300

    added first stack view with model data

--- QuizzicalQuiz/ViewControllers/QuestionsViewController.swift
+++ QuizzicalQuiz/ViewControllers/QuestionsViewController.swift
@@ -8,10 +8,85 @@
 import UIKit
 
 class QuestionsViewController: UIViewController {
-
+    
+    @IBOutlet var questionProgressView: UIProgressView!
+    @IBOutlet var questionLabel: UILabel!
+    
+    @IBOutlet var singleStackView: UIStackView!
+    @IBOutlet var singleButtons: [UIButton]!
+    
+    @IBOutlet var multipleStackView: UIStackView!
+    @IBOutlet var multipleLabels: [UILabel]!
+    @IBOutlet var multipleSwitches: [UISwitch]!
+    
+    @IBOutlet var rangedStackView: UIStackView!
+    @IBOutlet var rangedLabels: [UILabel]!
+    @IBOutlet var rangedSlider: UISlider!
+    
+    private let questions = Qustion.getQuestions()
+    private var currentAnswers: [Answer] {
+        questions[questionIndex].answers
+    }
+    private var questionIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        updateUI()
     }
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func multipleButtonPressed() {
+    }
+    
+    @IBAction func rangedAnswerButtonPressed() {
+    }
+}
 
+// MARK: - Private Methods
+extension QuestionsViewController {
+    private func updateUI() {
+        // Hide stacks
+        for stackView in [singleStackView,multipleStackView,rangedStackView] {
+            stackView?.isHidden = true
+        }
+        
+        // Get current question
+        let currentQuestion = questions[questionIndex]
+        
+        // Set current question for question label
+        questionLabel.text = currentQuestion.title
+        
+        // Caculate progress
+        let totalProgress = Float(questionIndex) / Float(questions.count)
+        
+        // Set progress for question progress view
+        questionProgressView.setProgress(totalProgress, animated: true)
+        
+        // Set navigation title
+        title = "Вопрос № \(questionIndex + 1) из \(questions.count)"
+        
+        showCurrentAnswers(for: currentQuestion.type)
+    }
+    
+    private func showCurrentAnswers(for type: ResponseType) {
+        switch type {
+        case .single: showSingleStackView(with: currentAnswers)
+
+        case .multiple: break
+            
+        case .ranged: break
+            
+        }
+    }
+    
+    private func showSingleStackView(with answers: [Answer]) {
+        singleStackView.isHidden.toggle()
+        
+        for (button, answer) in zip(singleButtons, answers) {
+            button.setTitle(answer.title, for: .normal)
+        }
+    
+    }
 }
